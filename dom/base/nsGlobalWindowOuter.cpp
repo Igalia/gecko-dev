c67ec80218b3fe676317acdbf17e3b162e708ac4
Sergio Villar Senin <svillar@igalia.com>
2022-06-06 16:52:52 +0200

    Fix YouTube media stopping when closing a window
    Whenever a YouTube video is played, and you open and close a new window,
    media playback stops. Opening a new window automagically resumes it.
    We can workaround this issue by not executing a very suspicious bunch
    of code in Gecko which apparently is really fragile.

--- dom/base/nsGlobalWindowOuter.cpp
+++ dom/base/nsGlobalWindowOuter.cpp
@@ -6049,6 +6049,7 @@ void nsGlobalWindowOuter::FinalClose() {
   // complete _two_ round-trips to the event loop before the call to
   // ReallyCloseWindow. This allows setTimeout handlers that are set after
   // FinalClose() is called to run before the window is torn down.
+  /*
   nsCOMPtr<nsPIDOMWindowInner> entryWindow =
       do_QueryInterface(GetEntryGlobal());
   bool indirect = entryWindow && entryWindow->GetOuterWindow() == this;
@@ -6057,6 +6058,7 @@ void nsGlobalWindowOuter::FinalClose() {
   } else {
     mHavePendingClose = true;
   }
+  */
 }
 
 void nsGlobalWindowOuter::ReallyCloseWindow() {
